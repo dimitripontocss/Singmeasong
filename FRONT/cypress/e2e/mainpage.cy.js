cdc7c65cd278aa04d0f7e5c0279b4ce74aa84d31
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-09-20 16:26:48 -0300

    Starting front end testing

--- FRONT/cypress/e2e/mainpage.cy.js
+++ FRONT/cypress/e2e/mainpage.cy.js
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Testing /", () => {
+  it("should create new recomendation", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.get('input[placeholder="Name"]').type("teste");
+    cy.get('input[placeholder="https://youtu.be/..."]').type("teste");
+    // cy.contains("svg").click();
+
+    cy.url().should("equal", "http://localhost:3000/");
+  });
+});
