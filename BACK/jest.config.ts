b5af398c99272ac40b862ab2b418d21f5349eaa3
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-09-22 19:09:08 -0300

    Changes on how the test run

--- BACK/jest.config.ts
+++ BACK/jest.config.ts
@@ -3,4 +3,16 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "test-config",
+    "interfaces",
+    "repositories",
+    "jestGlobalMocks.ts",
+    "<rootDir>/src/server.ts",
+    "<rootDir>/src/utils",
+    "<rootDir>/src/config",
+    "<rootDir>/tests/factories",
+    "<rootDir>/src/database.ts",
+  ],
 };
--- BACK/tests/serverTests.test.ts
+++
@@ -1,5 +0,0 @@
-import { unitTests } from "./unit/unitTests";
-import { integrationTests } from "./integration/app";
-
-integrationTests();
-unitTests();
