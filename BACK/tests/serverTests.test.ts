59bd2bd4dc25e977b35876b51025a3ad557107fd
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-09-22 16:04:02 -0300

    All tests are done

---
+++ BACK/tests/serverTests.test.ts
@@ -0,0 +1,5 @@
+import { unitTests } from "./unit/unitTests";
+import { integrationTests } from "./integration/app";
+
+integrationTests();
+unitTests();
--- FRONT/cypress/e2e/createRecommendation.cy.js
+++ FRONT/cypress/e2e/createRecommendation.cy.js
@@ -24,4 +24,36 @@ describe("Testing /", () => {
 
     cy.contains(newRecommendation.name).should("be.visible");
   });
+
+  it("should fail to create new recomendation with same name", () => {
+    const newRecommendation = {
+      name: faker.name.firstName(),
+      YTUrl: "https://www.youtube.com/watch?v=AyvLFfg9bPk",
+    };
+    cy.visit("http://localhost:3000/");
+
+    cy.get("#name").type(newRecommendation.name);
+    cy.get("#url").type(newRecommendation.YTUrl);
+    cy.intercept("POST", `http://localhost:5009/recommendations`).as(
+      "newRecom"
+    );
+
+    cy.get("#button").click();
+
+    cy.wait("@newRecom");
+
+    cy.get("#name").type(newRecommendation.name);
+    cy.get("#url").type(newRecommendation.YTUrl);
+    cy.intercept("POST", `http://localhost:5009/recommendations`).as(
+      "newRecom2"
+    );
+
+    cy.get("#button").click();
+
+    cy.wait("@newRecom2");
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("Error creating recommendation!");
+    });
+  });
 });
